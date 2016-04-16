5454eecbc3cdd9568a074a20adfd0eb57659cd88
ronald <ronald@blue-stream.co.za>
2016-04-16 11:12:29 +0200

    myFirst Commit

---
+++ MetopeMVCApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.MetopeNspace.Startup))]
+namespace ASP.MetopeNspace
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
