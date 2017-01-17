3472d11234faebf17ad108adf8b7a3d4f4b6a814
ronald <ronald@blue-stream.co.za>
2017-01-17 20:57:24 +0200

    SaveChanges

---
+++ MetopeMVCApp/Controllers/GridSecurityDetailController.cs
@@ -0,0 +1,29 @@
+﻿using MetopeMVCApp.Data;
+using MetopeMVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MetopeMVCApp.Controllers
+{
+    public class GridSecurityDetailController : Controller
+    {
+        private MetopeDbEntities db = new MetopeDbEntities();
+ 
+
+        //
+        // GET: /GridSecurityDetail/
+        public ActionResult Index()
+        {
+            IEnumerable<Security_Detail> security_detail = db.Security_Detail.ToList();
+
+            return View(security_detail);
+
+        } 
+         
+    }
+}
